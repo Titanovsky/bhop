9f6b6674c42003005291b6e2539e561aec0f8d57
Titanovsky <titanovskyteam@gmail.com>
2024-12-26 10:07:45 +0500

    update player

--- code/Player/Player.cs
+++ code/Player/Player.cs
@@ -1,62 +1,126 @@
-using Sandbox;
+﻿using Sandbox;
 
 public sealed class Player : Component
 {
 	[Property] private SauceController _sauceController;
-	[Property] private GameObject _map;
+	//[Property] private GameObject _map;
 	private List<Vector3> _spawns = new();
+
+	private GameObject _checkpoint;
+	private Vector3 _startPos = Vector3.Zero;
+	private Vector2 _startAng = Vector2.Zero;
+	private Vector3 _checkpointPos = Vector3.Zero;
+	private Vector2 _checkpointAng = Vector2.Zero;
 
 	protected override void OnStart()
 	{
-		PrepareSpawns();
 		PrepareControllers();
+		PrepareSpawns();
 	}
 
 	protected override void OnFixedUpdate()
 	{
-		CheckRespawnButton();
+		CheckResetButton();
+
+		//Log.Info( _body.WorldRotation.Angles() );
 	}
 
 	private void PrepareControllers()
 	{
 		if ( !_sauceController.IsValid() )
 			_sauceController = Components.Get<SauceController>();
+
+		//if ( !_body.IsValid() )
+			//_body = GameObject.Children.First();
 	}
 
-	private void CheckRespawnButton()
+	private void CheckResetButton()
 	{
 		if ( !Input.Pressed( "Reload" ) ) return;
 
+		ResetProgress();
 		Respawn();
 	}
 
 	private void PrepareSpawns()
 	{
-		if (!_map.IsValid())
-		{
-			Log.Error( "Where map/scene map?" );
+		_startPos = WorldPosition;
+		_startAng = _sauceController.LookAngle;
 
-			return;
-		}
+		//if (!_map.IsValid())
+		//{
+		//	Log.Error( "Where map/scene map?" );
 
-		foreach ( var gameobj in _map.Children )
-		{
-			if ( !gameobj.Components.Get<SpawnPoint>().IsValid() ) continue;
+		//	return;
+		//}
 
-			_spawns.Add( gameobj.WorldPosition );
+		//foreach ( var gameobj in _map.Children )
+		//{
+		//	if ( !gameobj.Components.Get<SpawnPoint>().IsValid() ) continue;
 
-			Log.Info( $"new spawn point {gameobj}" );
-		}
+		//	_spawns.Add( gameobj.WorldPosition );
+
+		//	Log.Info( $"new spawn point {gameobj}" );
+		//}
 	}
 
 	public void Respawn()
 	{
-		if ( _spawns.Count == 0 ) return;
+		//if ( _spawns.Count == 0 ) return;
 
-		var pos = _spawns[Game.Random.Next( _spawns.Count )];
-
-		WorldPosition = pos;
+		//var pos = _spawns[Game.Random.Next( _spawns.Count )];
 
 		_sauceController.Velocity = 0f;
+
+		if ( _checkpoint.IsValid() )
+		{
+			WorldPosition = _checkpointPos;
+			Rotate( _checkpointAng );
+		}
+		else
+		{
+			WorldPosition = _startPos;
+			Rotate(_startAng);
+		}
+
+	}
+
+	public void ResetProgress()
+	{
+		RemoveCheckpoint();
+	}
+
+	public void RemoveCheckpoint()
+	{
+		_checkpoint = null;
+	}
+
+	public void SetupCheckpoint(GameObject gameObj )
+	{
+		if ( !gameObj.IsValid() ) return;
+		if ( _checkpoint.IsValid() && _checkpoint == gameObj ) return;
+
+		var pos = GetSpawnPos( gameObj );
+
+		_checkpoint = gameObj;
+		_checkpointPos = pos;
+		_checkpointAng = _sauceController.LookAngle;
+
+		Log.Info( $"🎄 Checkpoint: {gameObj}" );
+	}
+
+	public void SetupCheckpoint( Collider collider )
+	{
+		SetupCheckpoint( collider.GameObject );
+	}
+
+	private Vector3 GetSpawnPos(GameObject gameObj)
+	{
+		return gameObj.GetBounds().Center;
+	}
+
+	private void Rotate(Vector2 ang)
+	{
+		_sauceController.LookAngle = ang;
 	}
 }
