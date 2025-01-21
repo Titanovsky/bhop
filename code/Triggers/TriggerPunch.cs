e0a73be572925c8e04cf6d6a2e6e66bd0aec72b4
Titanovsky <titanovskyteam@gmail.com>
2025-01-21 23:20:30 +0500

    rename TriggerVelocity to TriggerPunch

--- code/Triggers/TriggerPunch.cs
+++ code/Triggers/TriggerPunch.cs
@@ -1,6 +1,6 @@
 using Sandbox;
 
-public sealed class TriggerVelocity : Component, Component.ITriggerListener
+public sealed class TriggerPunch : Component, Component.ITriggerListener
 {
 	[Property, Description("Can use as Direction, if the values will be only 0f - 1f, and you will manipulate with Force")] public Vector3 positionForPunch = Vector3.Zero;
 	[Property, Description("Just not press 0f or a minus value")] public float force = 1f;
