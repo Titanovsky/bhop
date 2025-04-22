9c4e7ea67c1ec59ef1821c7ae38d4866e9f2d612
Titanovsky <titanovskyteam@gmail.com>
2025-04-22 07:37:47 +0500

    MaxId

--- code/Segment/Segment.cs
+++ code/Segment/Segment.cs
@@ -8,9 +8,14 @@ public class Segment
 	public float TimeStart { get; set; } = 0f;
 	public bool IsNow { get; set; } = false;
 
+	private static int MaxId = 0;
+
 	public Segment(int id)
 	{
+		if ( id >= MaxId ) Log.Warning( $"[Segment] Id {id} is alive" );
+
 		Id = id;
+		MaxId = id;
 	}
 
 	public bool IsDone() => (TimeDone > 0f);
